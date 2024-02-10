28db2fdb54a36283b438209ea4249ae476e1195b
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-10 14:26:06 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
-
-from sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Student, Base
 
 engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; 
+    Base.metadata.create_all(engine)
+    ipdb.set_trace()
